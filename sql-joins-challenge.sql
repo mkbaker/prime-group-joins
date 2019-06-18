5a4eb68a8b210d4ee02304cb0229b27113c7f0d9
Kellen <46249876+mkbaker@users.noreply.github.com>
2019-06-18 14:38:08 -0500

    Add files via upload

--- sql-joins-challenge.sql
+++ sql-joins-challenge.sql
@@ -46,3 +46,19 @@ SELECT SUM("on_hand")
 FROM "warehouse_product"
 JOIN "products" ON "warehouse_product"."product_id"="products"."id"
 WHERE "products"."description" = 'diet pepsi';
+
+--9. How much was the total cost for each order?
+SELECT "orders"."id", SUM("products"."unit_price")
+FROM "orders"
+JOIN "line_items" ON "orders"."id"="line_items"."order_id"
+JOIN "products" ON "line_items"."product_id"="products"."id"
+GROUP BY "orders"."id";
+
+--10. How much has each customer spent in total?
+SELECT "customers"."first_name", SUM("products"."unit_price")
+FROM "customers"
+    JOIN "addresses" ON "customers"."id"="addresses"."customer_id"
+    JOIN "orders" ON "addresses"."id"="orders"."address_id"
+    JOIN "line_items" ON "orders"."id"="line_items"."order_id"
+    JOIN "products" ON "line_items"."product_id"="products"."id"
+GROUP BY "customers"."first_name";
